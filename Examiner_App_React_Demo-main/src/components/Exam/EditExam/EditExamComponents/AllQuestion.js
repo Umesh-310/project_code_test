2d97653d49a57942e52c2b8a45842f03b451f8bc
Umesh-310 <umeshsaini8085@gmail.com>
2023-10-03 12:21:37 +0530

    [FE] - Added or Replace show Question to QuestionCard

--- Examiner_App_React_Demo-main/src/components/Exam/EditExam/EditExamComponents/AllQuestion.js
+++ Examiner_App_React_Demo-main/src/components/Exam/EditExam/EditExamComponents/AllQuestion.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import css from "../EditExam.module.css";
 import { Box, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { GetAllQuestion } from "../../../../store/questionSlice";
+import QuestionCard from "./QuestionCard";
+import { questionCard_Type } from "../../../../utils/utils";
 
-const AllQuestion = () => {
+const AllQuestion = (props) => {
+  const { getExamDetail, addQuestion } = props;
+  const dispatch = useDispatch();
+  const { allQuestion, loading } = useSelector((state) => state.question);
+
+  useEffect(() => {
+    dispatch(GetAllQuestion());
+  }, [dispatch]);
   return (
     <Box className={css.gridBox}>
       <Box className={css.topHeading}>
         <Typography className={css.topHeadingText}>
           Challenge library
         </Typography>
-        
+      </Box>
+      <Box className={css.queCardBox}>
+        {allQuestion &&
+          allQuestion.map((question, i) => (
+            <QuestionCard
+              key={i}
+              question={question}
+              SelectedChallenge={questionCard_Type.LIBRARY}
+              getExamDetail={getExamDetail}
+              onClick={addQuestion}
+            />
+          ))}
       </Box>
     </Box>
   );
--- Examiner_App_React_Demo-main/src/components/Question/AllQuestion/QuestionTable3.js
+++ Examiner_App_React_Demo-main/src/components/Question/AllQuestion/QuestionTable3.js
@@ -1,221 +1,11 @@
-import { memo, useEffect, useState, useCallback, Fragment } from "react";
+import { memo } from "react";
 
-import PropTypes from "prop-types";
-import {
-  Box,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  TableSortLabel,
-  Paper,
-} from "@mui/material";
-import { visuallyHidden } from "@mui/utils";
+import { Box, Grid } from "@mui/material";
 
-import QuestionDetailModal from "./QuestionDetailModal";
-import { LevelBage } from "../../../utils/LevelBage";
-
-function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function getComparator(order, orderBy) {
-  return order === "desc"
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
-function stableSort(array, comparator) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-const headCells = [
-  {
-    id: "title",
-    numeric: false,
-    label: "Title",
-    align: "left",
-    minWidth: 170,
-  },
-  {
-    id: "level",
-    numeric: false,
-    label: "Level",
-    align: "center",
-    minWidth: 170,
-  },
-  {
-    id: "created_by",
-    numeric: false,
-    label: "Created By",
-    align: "center",
-    minWidth: 170,
-  },
-  {
-    id: "action",
-    numeric: false,
-    label: "Action",
-    align: "center",
-  },
-];
-
-const DEFAULT_ORDER = "asc";
-const DEFAULT_ORDER_BY = "updated_at";
-const DEFAULT_ROWS_PER_PAGE = 10;
-
-function EnhancedTableHead(props) {
-  const { order, orderBy, rowCount, onRequestSort } = props;
-  const createSortHandler = (newOrderBy) => (event) => {
-    onRequestSort(event, newOrderBy);
-  };
-
-  return (
-    <TableHead>
-      <TableRow>
-        {headCells.map((headCell) => (
-          <TableCell
-            style={{
-              minWidth: headCell.minWidth,
-              fontWeight: 900,
-              fontSize: "18px",
-            }}
-            key={headCell.id}
-            align={headCell.align}
-            sortDirection={orderBy === headCell.id ? order : false}
-          >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </Box>
-              ) : null}
-            </TableSortLabel>
-          </TableCell>
-        ))}
-      </TableRow>
-    </TableHead>
-  );
-}
-
-EnhancedTableHead.propTypes = {
-  onRequestSort: PropTypes.func.isRequired,
-  order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-  orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
-};
+import QuestionCard from "../../Exam/EditExam/EditExamComponents/QuestionCard";
+import { questionCard_Type } from "../../../utils/utils";
 
 function EnhancedTable({ data: rows }) {
-  const [order, setOrder] = useState(DEFAULT_ORDER);
-  const [orderBy, setOrderBy] = useState(DEFAULT_ORDER_BY);
-  const [page, setPage] = useState(0);
-  const [visibleRows, setVisibleRows] = useState(null);
-  const [rowsPerPage, setRowsPerPage] = useState(DEFAULT_ROWS_PER_PAGE);
-  const [paddingHeight, setPaddingHeight] = useState(0);
-
-  useEffect(() => {
-    let rowsOnMount = stableSort(
-      rows,
-      getComparator(DEFAULT_ORDER, DEFAULT_ORDER_BY)
-    );
-
-    rowsOnMount = rowsOnMount.slice(
-      0 * DEFAULT_ROWS_PER_PAGE,
-      0 * DEFAULT_ROWS_PER_PAGE + DEFAULT_ROWS_PER_PAGE
-    );
-    setVisibleRows(rowsOnMount);
-  }, [rows]);
-
-  const handleRequestSort = useCallback(
-    (event, newOrderBy) => {
-      const isAsc = orderBy === newOrderBy && order === "asc";
-      const toggledOrder = isAsc ? "desc" : "asc";
-      setOrder(toggledOrder);
-      setOrderBy(newOrderBy);
-
-      const sortedRows = stableSort(
-        rows,
-        getComparator(toggledOrder, newOrderBy)
-      );
-      const updatedRows = sortedRows.slice(
-        page * rowsPerPage,
-        page * rowsPerPage + rowsPerPage
-      );
-      setVisibleRows(updatedRows);
-    },
-    [order, orderBy, page, rowsPerPage, rows]
-  );
-
-  const handleChangePage = useCallback(
-    (event, newPage) => {
-      setPage(newPage);
-
-      const sortedRows = stableSort(rows, getComparator(order, orderBy));
-      const updatedRows = sortedRows.slice(
-        newPage * rowsPerPage,
-        newPage * rowsPerPage + rowsPerPage
-      );
-
-      setVisibleRows(updatedRows);
-
-      // Avoid a layout jump when reaching the last page with empty rows.
-      const numEmptyRows =
-        newPage > 0
-          ? Math.max(0, (1 + newPage) * rowsPerPage - rows.length)
-          : 0;
-
-      const newPaddingHeight = 33 * numEmptyRows;
-      setPaddingHeight(newPaddingHeight);
-    },
-    [order, orderBy, rowsPerPage, rows]
-  );
-
-  const handleChangeRowsPerPage = useCallback(
-    (event) => {
-      const updatedRowsPerPage = parseInt(event.target.value, 10);
-      setRowsPerPage(updatedRowsPerPage);
-
-      setPage(0);
-
-      const sortedRows = stableSort(rows, getComparator(order, orderBy));
-      const updatedRows = sortedRows.slice(
-        0 * updatedRowsPerPage,
-        0 * updatedRowsPerPage + updatedRowsPerPage
-      );
-
-      setVisibleRows(updatedRows);
-
-      // There is no layout jump to handle on the first page.
-      setPaddingHeight(0);
-    },
-    [order, orderBy, rows]
-  );
-
   if (rows.length <= 0) {
     return (
       <Box sx={{ width: "100%", textAlign: "center" }}>
@@ -224,70 +14,19 @@ function EnhancedTable({ data: rows }) {
     );
   } else {
     return (
-      <Box sx={{ width: "100%" }}>
-        <Paper sx={{ width: "100%", mb: 2 }}>
-          <TableContainer>
-            <Table
-              sx={{ minWidth: 750 }}
-              aria-labelledby="tableTitle"
-              size={"small"}
-            >
-              <EnhancedTableHead
-                order={order}
-                orderBy={orderBy}
-                onRequestSort={handleRequestSort}
-                rowCount={rows.length}
-              />
-              <TableBody>
-                {visibleRows
-                  ? visibleRows.map((row, index) => {
-                      return (
-                        <Fragment key={row.id}>
-                          <TableRow
-                            hover
-                            role="checkbox"
-                            tabIndex={-1}
-                            key={row.name}
-                            sx={{ cursor: "pointer" }}
-                          >
-                            <TableCell component="th">{row.title}</TableCell>
-                            <TableCell align="center">
-                              <LevelBage level={row?.level} />
-                            </TableCell>
-                            <TableCell align="center">
-                              {row?.created_by?.name}
-                            </TableCell>
-                            <TableCell align="center">
-                              <QuestionDetailModal que={row} />
-                            </TableCell>
-                          </TableRow>
-                        </Fragment>
-                      );
-                    })
-                  : null}
-                {paddingHeight > 0 && (
-                  <TableRow
-                    style={{
-                      height: paddingHeight,
-                    }}
-                  >
-                    <TableCell colSpan={6} />
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <TablePagination
-            rowsPerPageOptions={[5, 10, 25]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-          />
-        </Paper>
-      </Box>
+      <Grid container spacing={3}>
+        {rows &&
+          rows.map((row, index) => {
+            return (
+              <Grid item xs={6} key={index}>
+                <QuestionCard
+                  question={row}
+                  SelectedChallenge={questionCard_Type.ALL_QUESTION}
+                />
+              </Grid>
+            );
+          })}
+      </Grid>
     );
   }
 }
